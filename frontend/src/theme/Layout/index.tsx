5f6ee13e6aee58147b70ed96b96b4f15ca28f338
Shumaila Yousuf <shumailayousuf1000@gmail.com>
2026-01-25 11:52:28 +0500

    Fix Docusaurus layout path to render ChatWidget

---
+++ frontend/src/theme/Layout/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import OriginalLayout from '@theme-original/Layout';
+import ChatWidget from '../../components/ChatWidget/ChatWidget';
+
+export default function Layout(props) {
+  return (
+    <>
+      <OriginalLayout {...props} />
+      <ChatWidget />
+    </>
+  );
+}
